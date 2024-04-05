91c13d865ed270f3b2ebc657928cb6655c41dc35
Sagar Gupta <sagargupta.gola@gmail.com>
2024-04-05 11:09:17 +0530

    done with app.js file

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ import bodyParser from 'body-parser';
 import db from './Db/db.js';
 import eventRouter from './Router/Event.js';
 import { configDotenv } from 'dotenv';
+require('dotenv').config();
 
 
 const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+
+//Mounting the events router 
+
+app.use('/events', eventRouter);
+
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  });
+  
+  db.on('error', err => {
+    console.error('MongoDB connection error:', err);
+  });
